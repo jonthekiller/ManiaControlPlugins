19df62cf7efb1fbc59c862e48bf9a220f1d1abc3
jonthekiller <jonthekiller@users.noreply.github.com>
2019-03-16 13:11:00 +0100

    New features
    Add 2 commands to allow an admin to unmute and unban a players on all synchronized servers at the same time.

--- Drakonia/ModerationSyncPlugin.php
+++ Drakonia/ModerationSyncPlugin.php
@@ -8,33 +8,51 @@
 
 namespace Drakonia;
 
+use FML\Controls\Frame;
+use FML\Controls\Labels\Label_Button;
+use FML\Controls\Labels\Label_Text;
+use FML\Controls\Quads\Quad_BgRaceScore2;
+use FML\Controls\Quads\Quad_BgsPlayerCard;
+use FML\ManiaLink;
+use FML\Script\Features\Paging;
+use FML\Script\Script;
+use ManiaControl\Admin\AuthenticationManager;
 use ManiaControl\Callbacks\CallbackListener;
-use ManiaControl\Callbacks\Callbacks;
+use ManiaControl\Callbacks\CallbackManager;
 use ManiaControl\Callbacks\TimerListener;
+use ManiaControl\Commands\CommandListener;
 use ManiaControl\Logger;
 use ManiaControl\ManiaControl;
+use ManiaControl\Manialinks\LabelLine;
+use ManiaControl\Manialinks\ManialinkManager;
 use ManiaControl\Players\Player;
 use ManiaControl\Players\PlayerManager;
 use ManiaControl\Plugins\Plugin;
 use ManiaControl\Settings\Setting;
 use ManiaControl\Settings\SettingManager;
 use Maniaplanet\DedicatedServer\InvalidArgumentException;
-use Maniaplanet\DedicatedServer\Xmlrpc\AlreadyInListException;
 use Maniaplanet\DedicatedServer\Xmlrpc\NotInListException;
 use Maniaplanet\DedicatedServer\Xmlrpc\UnknownPlayerException;
 
-class ModerationSyncPlugin implements CallbackListener, TimerListener, Plugin
+class ModerationSyncPlugin implements CallbackListener, TimerListener, CommandListener, Plugin
 {
 
     /*
 * Constants
 */
     const PLUGIN_ID = 130;
-    const PLUGIN_VERSION = 0.21;
+    const PLUGIN_VERSION = 0.4;
     const PLUGIN_NAME = 'ModerationSyncPlugin';
     const PLUGIN_AUTHOR = 'jonthekiller';
 
     const SETTING_MODERATIONSYNC_ACTIVATED = 'ModerationSyncPlugin Activated';
+    const SETTING_MODO_AUTHLEVEL = 'Auth level for the modo* commands:';
+    const SHOWN_MAIN_WINDOW           = -1;
+    const MAX_PLAYERS_PER_PAGE        = 15;
+    const MAX_PAGES_PER_CHUNK         = 2;
+    const ACTION_PAGING_CHUNKS        = 'PlayerList.PagingChunks';
+    const ACTION_UNBAN   = 'ModoList.UnbanPlayer';
+    const ACTION_UNIGNORE   = 'ModoList.UnignorePlayer';
 
     /** @var ManiaControl $maniaControl */
     private $maniaControl = null;
@@ -98,18 +116,52 @@ class ModerationSyncPlugin implements CallbackListener, TimerListener, Plugin
         // Settings
         $this->maniaControl->getSettingManager()->initSetting($this, self::SETTING_MODERATIONSYNC_ACTIVATED, true);
 
+        $this->maniaControl->getSettingManager()->initSetting(
+            $this,
+            self::SETTING_MODO_AUTHLEVEL,
+            AuthenticationManager::AUTH_LEVEL_ADMIN
+        );
+
         // Callbacks
         $this->maniaControl->getCallbackManager()->registerCallbackListener(
             PlayerManager::CB_PLAYERCONNECT,
             $this,
             'handlePlayerConnect'
         );
+
+        $this->maniaControl->getCallbackManager()->registerCallbackListener(
+            CallbackManager::CB_MP_PLAYERCHAT,
+            $this,
+            'handlePlayerChat'
+        );
+
+        $this->maniaControl->getCallbackManager()->registerCallbackListener(CallbackManager::CB_MP_PLAYERMANIALINKPAGEANSWER, $this, 'handleManialinkPageAnswer');
+
         $this->maniaControl->getCallbackManager()->registerCallbackListener(SettingManager::CB_SETTING_CHANGED, $this, 'updateSettings');
 
         $this->maniaControl->getTimerManager()->registerTimerListening($this, 'handle60Seconds', 60000);
 
         $this->initTables();
         $this->init();
+
+        $this->maniaControl->getClient()->chatEnableManualRouting(true, true);
+
+
+        $this->maniaControl->getCommandManager()->registerCommandListener(
+            'modobanlist',
+            $this,
+            'onCommandModoBanList',
+            true,
+            'Show ban sync list'
+        );
+
+        $this->maniaControl->getCommandManager()->registerCommandListener(
+            'modomutelist',
+            $this,
+            'onCommandModoIgnoreList',
+            true,
+            'Show mute sync list'
+        );
 
         return true;
 
@@ -160,6 +212,22 @@ class ModerationSyncPlugin implements CallbackListener, TimerListener, Plugin
     }
 
 
+    public function handlePlayerChat($callback) {
+        $args = $callback[1];
+        $nick = $this->maniaControl->getPlayerManager()->getPlayer($args[1])->nickname;
+
+        if (substr($args[2], 0, 1) != '/') {
+            if ($args[0] != 0) {
+                try {
+                    $this->maniaControl->getClient()->chatForward('$z' . $args[2] . '$z', $args[1]);
+                } catch (InvalidArgumentException $e) {
+                    var_dump($e);
+                }
+            }
+        }
+
+    }
+
     public function handlePlayerConnect(Player $player)
     {
         // Check if force ignore
@@ -207,7 +275,7 @@ class ModerationSyncPlugin implements CallbackListener, TimerListener, Plugin
 
     public function manageIgnoreList()
     {
-        Logger::log("Refresh Ignorelist");
+//        Logger::log("Refresh Ignorelist");
         $mysqli = $this->maniaControl->getDatabase()->getMysqli();
         // Get the ignorelist from the database
         $query = "SELECT `playerIndex` FROM `drakonia_ignorelist`
@@ -321,7 +389,7 @@ class ModerationSyncPlugin implements CallbackListener, TimerListener, Plugin
 
     public function manageBanList()
     {
-        Logger::log("Refresh Banlist");
+//        Logger::log("Refresh Banlist");
         $mysqli = $this->maniaControl->getDatabase()->getMysqli();
         // Get the banlist from the database
         $query = "SELECT `playerIndex` FROM `drakonia_banlist`
@@ -435,6 +503,349 @@ class ModerationSyncPlugin implements CallbackListener, TimerListener, Plugin
 
     }
 
+
+    public function onCommandModoBanList(array $chatCallback, Player $player)
+    {
+        $authLevel = $this->maniaControl->getSettingManager()->getSettingValue($this, self::SETTING_MODO_AUTHLEVEL);
+        if (!$this->maniaControl->getAuthenticationManager()->checkRight($player, $authLevel)) {
+            $this->maniaControl->getAuthenticationManager()->sendNotAllowed($player);
+
+            return;
+        }
+        $this->ModoBanList($player);
+    }
+
+    public function onCommandModoIgnoreList (array $chatCallback, Player $player)
+    {
+        $authLevel = $this->maniaControl->getSettingManager()->getSettingValue($this, self::SETTING_MODO_AUTHLEVEL);
+        if (!$this->maniaControl->getAuthenticationManager()->checkRight($player, $authLevel)) {
+            $this->maniaControl->getAuthenticationManager()->sendNotAllowed($player);
+
+            return;
+        }
+        $this->ModoIgnoreList($player);
+    }
+
+
+    public function ModoBanList(Player $player)
+    {
+        $width  = $this->maniaControl->getManialinkManager()->getStyleManager()->getListWidgetsWidth();
+        $height = $this->maniaControl->getManialinkManager()->getStyleManager()->getListWidgetsHeight();
+
+        $mysqli = $this->maniaControl->getDatabase()->getMysqli();
+        $query = "SELECT `playerIndex` FROM `drakonia_banlist`
+					WHERE `banned` = 1;";
+//        Logger::log($query);
+        $result = $mysqli->query($query);
+        if ($mysqli->error) {
+            trigger_error($mysqli->error);
+            return null;
+        }
+        $bans = array();
+        while ($ban = $result->fetch_object()) {
+            array_push($bans, $ban);
+        }
+        $result->free();
+
+
+        //Create ManiaLink
+        $maniaLink = new ManiaLink(ManialinkManager::MAIN_MLID);
+        $paging    = new Paging();
+        $script    = new Script();
+        $script->addFeature($paging);
+        $maniaLink->setScript($script);
+
+        // Main frame
+        $frame = $this->maniaControl->getManialinkManager()->getStyleManager()->getDefaultListFrame($script, $paging);
+        $maniaLink->addChild($frame);
+
+        // Start offsets
+        $posX = -$width / 2;
+        $posY = $height / 2;
+
+        //Predefine description Label
+        $descriptionLabel = $this->maniaControl->getManialinkManager()->getStyleManager()->getDefaultDescriptionLabel();
+        $frame->addChild($descriptionLabel);
+
+        // Headline
+        $headFrame = new Frame();
+        $frame->addChild($headFrame);
+        $headFrame->setY($posY - 5);
+
+        $labelLine = new LabelLine($headFrame);
+        $labelLine->addLabelEntryText('Id', $posX + 5);
+        $labelLine->addLabelEntryText('Nickname', $posX + 18);
+        $labelLine->addLabelEntryText('Login', $posX + 70);
+        $labelLine->addLabelEntryText('Actions', $posX + 120);
+        $labelLine->render();
+
+        $index     = 1;
+        $posY      -= 10;
+        $pageFrame = null;
+
+        foreach ($bans as $ban) {
+
+            $target = $this->maniaControl->getPlayerManager()->getPlayerByIndex($ban->playerIndex);
+
+            if ($index % self::MAX_PLAYERS_PER_PAGE === 1) {
+                $pageFrame = new Frame();
+                $frame->addChild($pageFrame);
+
+                $paging->addPageControl($pageFrame);
+                $posY = $height / 2 - 10;
+            }
+
+            $playerFrame = new Frame();
+            $pageFrame->addChild($playerFrame);
+            $playerFrame->setY($posY);
+
+            if ($index % 2 !== 0) {
+                $lineQuad = new Quad_BgsPlayerCard();
+                $playerFrame->addChild($lineQuad);
+                $lineQuad->setSize($width, 4);
+                $lineQuad->setSubStyle($lineQuad::SUBSTYLE_BgPlayerCardBig);
+                $lineQuad->setZ(-0.1);
+            }
+
+            $labelLine = new LabelLine($playerFrame);
+            $labelLine->addLabelEntryText($index, $posX + 5, 13);
+            $labelLine->addLabelEntryText($target->nickname, $posX + 18, 52);
+            $labelLine->addLabelEntryText($target->login, $posX + 70, 48);
+            $labelLine->render();
+
+            // Level Quad
+            $rightQuad = new Quad_BgRaceScore2();
+            $playerFrame->addChild($rightQuad);
+            $rightQuad->setX($posX + 13);
+            $rightQuad->setZ(5);
+            $rightQuad->setSubStyle($rightQuad::SUBSTYLE_CupFinisher);
+            $rightQuad->setSize(7, 3.5);
+
+            $rightLabel = new Label_Text();
+            $playerFrame->addChild($rightLabel);
+            $rightLabel->setX($posX + 13.9);
+            $rightLabel->setTextSize(0.8);
+            $rightLabel->setZ(10);
+            $rightLabel->setText($this->maniaControl->getAuthenticationManager()->getAuthLevelAbbreviation($target));
+            $description = $this->maniaControl->getAuthenticationManager()->getAuthLevelName($target) . " " . $target->nickname;
+            $rightLabel->addTooltipLabelFeature($descriptionLabel, $description);
+
+                //Settings
+                $style      = Label_Text::STYLE_TextCardSmall;
+                $textColor  = 'FFF';
+                $quadWidth  = 24;
+                $quadHeight = 3.4;
+
+                // Quad
+                $quad = new Quad_BgsPlayerCard();
+                $playerFrame->addChild($quad);
+                $quad->setZ(11);
+                $quad->setX($posX + 130);
+                $quad->setSubStyle($quad::SUBSTYLE_BgPlayerCardBig);
+                $quad->setSize($quadWidth, $quadHeight);
+                $quad->setAction(self::ACTION_UNBAN . "." . $target->login);
+
+                //Label
+                $label = new Label_Button();
+                $playerFrame->addChild($label);
+                $label->setX($posX + 130);
+                $quad->setZ(12);
+                $label->setStyle($style);
+                $label->setTextSize(1);
+                $label->setTextColor($textColor);
+                $label->setText("Unban");
+
+            $posY -= 4;
+            $index++;
+        }
+
+        // Render and display xml
+        $this->maniaControl->getManialinkManager()->displayWidget($maniaLink, $player, 'ModoBanList');
+
+
+    }
+
+    public function ModoIgnoreList(Player $player)
+    {
+        $width  = $this->maniaControl->getManialinkManager()->getStyleManager()->getListWidgetsWidth();
+        $height = $this->maniaControl->getManialinkManager()->getStyleManager()->getListWidgetsHeight();
+
+        $mysqli = $this->maniaControl->getDatabase()->getMysqli();
+        $query = "SELECT `playerIndex` FROM `drakonia_ignorelist`
+					WHERE `ignored` = 1;";
+//        Logger::log($query);
+        $result = $mysqli->query($query);
+        if ($mysqli->error) {
+            trigger_error($mysqli->error);
+            return null;
+        }
+        $ignores = array();
+        while ($ignore = $result->fetch_object()) {
+            array_push($ignores, $ignore);
+        }
+        $result->free();
+
+
+        //Create ManiaLink
+        $maniaLink = new ManiaLink(ManialinkManager::MAIN_MLID);
+        $paging    = new Paging();
+        $script    = new Script();
+        $script->addFeature($paging);
+        $maniaLink->setScript($script);
+
+        // Main frame
+        $frame = $this->maniaControl->getManialinkManager()->getStyleManager()->getDefaultListFrame($script, $paging);
+        $maniaLink->addChild($frame);
+
+        // Start offsets
+        $posX = -$width / 2;
+        $posY = $height / 2;
+
+        //Predefine description Label
+        $descriptionLabel = $this->maniaControl->getManialinkManager()->getStyleManager()->getDefaultDescriptionLabel();
+        $frame->addChild($descriptionLabel);
+
+        // Headline
+        $headFrame = new Frame();
+        $frame->addChild($headFrame);
+        $headFrame->setY($posY - 5);
+
+        $labelLine = new LabelLine($headFrame);
+        $labelLine->addLabelEntryText('Id', $posX + 5);
+        $labelLine->addLabelEntryText('Nickname', $posX + 18);
+        $labelLine->addLabelEntryText('Login', $posX + 70);
+        $labelLine->addLabelEntryText('Actions', $posX + 120);
+        $labelLine->render();
+
+        $index     = 1;
+        $posY      -= 10;
+        $pageFrame = null;
+
+        foreach ($ignores as $ignore) {
+
+            $target = $this->maniaControl->getPlayerManager()->getPlayerByIndex($ignore->playerIndex);
+
+            if ($index % self::MAX_PLAYERS_PER_PAGE === 1) {
+                $pageFrame = new Frame();
+                $frame->addChild($pageFrame);
+
+                $paging->addPageControl($pageFrame);
+                $posY = $height / 2 - 10;
+            }
+
+            $playerFrame = new Frame();
+            $pageFrame->addChild($playerFrame);
+            $playerFrame->setY($posY);
+
+            if ($index % 2 !== 0) {
+                $lineQuad = new Quad_BgsPlayerCard();
+                $playerFrame->addChild($lineQuad);
+                $lineQuad->setSize($width, 4);
+                $lineQuad->setSubStyle($lineQuad::SUBSTYLE_BgPlayerCardBig);
+                $lineQuad->setZ(-0.1);
+            }
+
+            $labelLine = new LabelLine($playerFrame);
+            $labelLine->addLabelEntryText($index, $posX + 5, 13);
+            $labelLine->addLabelEntryText($target->nickname, $posX + 18, 52);
+            $labelLine->addLabelEntryText($target->login, $posX + 70, 48);
+            $labelLine->render();
+
+            // Level Quad
+            $rightQuad = new Quad_BgRaceScore2();
+            $playerFrame->addChild($rightQuad);
+            $rightQuad->setX($posX + 13);
+            $rightQuad->setZ(5);
+            $rightQuad->setSubStyle($rightQuad::SUBSTYLE_CupFinisher);
+            $rightQuad->setSize(7, 3.5);
+
+            $rightLabel = new Label_Text();
+            $playerFrame->addChild($rightLabel);
+            $rightLabel->setX($posX + 13.9);
+            $rightLabel->setTextSize(0.8);
+            $rightLabel->setZ(10);
+            $rightLabel->setText($this->maniaControl->getAuthenticationManager()->getAuthLevelAbbreviation($target));
+            $description = $this->maniaControl->getAuthenticationManager()->getAuthLevelName($target) . " " . $target->nickname;
+            $rightLabel->addTooltipLabelFeature($descriptionLabel, $description);
+
+            //Settings
+            $style      = Label_Text::STYLE_TextCardSmall;
+            $textColor  = 'FFF';
+            $quadWidth  = 24;
+            $quadHeight = 3.4;
+
+            // Quad
+            $quad = new Quad_BgsPlayerCard();
+            $playerFrame->addChild($quad);
+            $quad->setZ(11);
+            $quad->setX($posX + 130);
+            $quad->setSubStyle($quad::SUBSTYLE_BgPlayerCardBig);
+            $quad->setSize($quadWidth, $quadHeight);
+            $quad->setAction(self::ACTION_UNIGNORE . "." . $target->login);
+
+            //Label
+            $label = new Label_Button();
+            $playerFrame->addChild($label);
+            $label->setX($posX + 130);
+            $quad->setZ(12);
+            $label->setStyle($style);
+            $label->setTextSize(1);
+            $label->setTextColor($textColor);
+            $label->setText("Unmute");
+
+            $posY -= 4;
+            $index++;
+        }
+
+        // Render and display xml
+        $this->maniaControl->getManialinkManager()->displayWidget($maniaLink, $player, 'ModoIgnoreList');
+
+
+    }
+
+
+
+    /**
+     * Called on ManialinkPageAnswer
+     *
+     * @internal
+     * @param array $callback
+     */
+    public function handleManialinkPageAnswer(array $callback) {
+        $actionId    = $callback[1][2];
+        $actionArray = explode('.', $actionId, 3);
+        if (count($actionArray) <= 2) {
+            return;
+        }
+
+        $action      = $actionArray[0] . '.' . $actionArray[1];
+        $targetLogin = $actionArray[2];
+
+        $player = $this->maniaControl->getPlayerManager()->getPlayer($targetLogin);
+        switch ($action) {
+            case self::ACTION_UNBAN:
+                $mysqli = $this->maniaControl->getDatabase()->getMysqli();
+                $query = "UPDATE `drakonia_banlist` set `banned` = 0 where `playerIndex` = " .$player->index;
+                $result = $mysqli->query($query);
+                if ($mysqli->error) {
+                    trigger_error($mysqli->error);
+                    return null;
+                }
+                $this->maniaControl->getChat()->sendSuccessToAdmins("Player ". $player->getEscapedNickname() . " has been unbanned!");
+                break;
+            case self::ACTION_UNIGNORE:
+                $mysqli = $this->maniaControl->getDatabase()->getMysqli();
+                $query = "UPDATE `drakonia_ignorelist` set `ignored` = 0 where `playerIndex` = " .$player->index;
+                $result = $mysqli->query($query);
+                if ($mysqli->error) {
+                    trigger_error($mysqli->error);
+                    return null;
+                }
+                $this->maniaControl->getChat()->sendSuccessToAdmins("Player ". $player->getEscapedNickname() . " has been unmuted!");
+                break;
+        }
+    }
+
     /**
      * Update Widgets on Setting Changes
      *
@@ -452,5 +863,6 @@ class ModerationSyncPlugin implements CallbackListener, TimerListener, Plugin
      */
     public function unload()
     {
+        $this->maniaControl->getClient()->chatEnableManualRouting(false);
     }
 }
